89d725f9407eac10229d0cdaa15f94208fed718e
srinivasan <srinivasan@srinivasan-Lenovo-G510>
2015-01-04 15:35:23 +0530

    Convert Array to Map

--- AmazingJava/src/main/java/com/srini/basic/AmazingJava/ConvertArrayToMap.java
+++ AmazingJava/src/main/java/com/srini/basic/AmazingJava/ConvertArrayToMap.java
@@ -1,7 +1,16 @@
 package com.srini.basic.AmazingJava;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 public class ConvertArrayToMap {
 	public static void main(String[] args) {
+		String[][] countries = { { "United States", "New York" },
+				{ "United Kingdom", "London" }, { "Netherland", "Amsterdam" },
+				{ "Japan", "Tokyo" }, { "France", "Paris" } };
+		Map<?, ?> map = ArrayUtils.toMap(countries);
+		System.out.println("Capital of Japan is " + map.get("Japan"));
 
 	}
 
