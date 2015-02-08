cfefdff0f99effeb9f811ef9b648174e57993fb5
srinivasan <srinivasan@srinivasan-Lenovo-G510>
2015-02-08 10:35:25 +0530

    New pythonproject

---
+++ Pythonbuilt/com/srini/stringtest/First.py
@@ -0,0 +1,26 @@
+'''
+Created on 08-Feb-2015
+
+@author: srinivasan
+'''
+import string
+print string.ascii_letters
+
+# format
+sub = "python string!"
+s = 'hello {0}'
+print s.format(sub)
+
+# Calculating the min and max from dictionary
+prices = {
+'ACME': 45.23,
+'AAPL': 612.78,
+'IBM': 205.55,
+'HPQ': 37.20,
+'FB': 10.75
+}
+print min(zip(prices.values(), prices.keys()))
+print max(zip(prices.values(), prices.keys()))
+# soreted based on values
+print sorted(zip(prices.values(), prices.keys()))
+
